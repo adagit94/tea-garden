fd2c077c0ec43bcb403fd162e4d0a41552394712
adagit94 <56156969+adagit94@users.noreply.github.com>
2020-09-02 21:51:25 +0200

    vercel hosting sync

--- components/layout/main/Main.jsx
+++ components/layout/main/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 
 export default function Main({ page }) {
+  console.log(page);
+  
   return (
     <main>
       <Container fluid={page.type.name === 'Index'}>{page}</Container>
