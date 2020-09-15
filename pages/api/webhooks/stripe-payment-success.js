0d1e60aed42ae7547509025c413e10a2c922d737
adagit94 <56156969+adagit94@users.noreply.github.com>
2020-09-15 12:36:50 +0200

    vercel sync

--- pages/api/webhooks/stripe-payment-success.js
+++ pages/api/webhooks/stripe-payment-success.js
@@ -1,10 +1,9 @@
 export default function (req, res) {
   const event = req.body;
 
-  
-  console.log(event.data.object);
-  console.log(event.data.object.metadata);
   res.status(200).json({ received: true });
+  
+  console.log(event.data.object.metadata);
 }
 
 //orderDataRes.paymentConfirmed = true;
