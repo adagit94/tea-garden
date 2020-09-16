fc73814598bd58cdd926dfc6c9d039dec6c9a16f
adagit94 <56156969+adagit94@users.noreply.github.com>
2020-09-16 10:12:12 +0200

    vercel sync

--- pages/api/webhooks/stripe-payment-success.js
+++ pages/api/webhooks/stripe-payment-success.js
@@ -10,7 +10,7 @@ export default async function (req, res) {
     products: JSON.parse(metadata.products),
   };
 
-  saveOrder(orderData);
+  await saveOrder(orderData);
   sendOrder(orderData);
 
   res.status(200).json({ received: true });
